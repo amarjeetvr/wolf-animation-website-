5fb148ac8fc6a09bc36ba2c243f3f1c85ee1051d
amarjeetvr <av457508@gmail.com>
2026-01-08 12:33:35 +0530

    position set of wolf model

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import{Canvas, useThree}from'@react-three/fiber';
-import { OrbitControls ,useGLTF} from '@react-three/drei';
+import { Canvas, useThree } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
 
 const Dog = () => {
-// animation 3d add
-  const model  = useGLTF('/models/dog.drc.glb');
-   
+  // animation 3d
+  const model = useGLTF('/models/dog.drc.glb');
+
+  useThree(({ camera,scene,gl }) => {
+    console.log(camera.position);
+    camera.position.z = -0.7;
+  })
 
   return (
-   <>
-      <primitive object={model.scene}   position={[0,0,0]} />
+    <>
+      <primitive object={model.scene} position={[0, -0.5, 0]} />
 
 
-    <directionalLight position={[0,5,5]} color={0xffffff} intensity={10} />
-    <OrbitControls />
+      <directionalLight position={[0, 5, 5]} color={0xffffff} intensity={10} />
+      <OrbitControls />
     </>
   )
 }
