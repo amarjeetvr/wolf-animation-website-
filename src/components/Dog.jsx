c609c344760d20d01da8ad9eb3b27e6194d41b6d
amarjeetvr <av457508@gmail.com>
2026-01-08 12:50:31 +0530

    normal img add

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -8,12 +8,12 @@ const Dog = () => {
 
   useThree(({ camera,scene,gl }) => {
     console.log(camera.position);
-    camera.position.z = -0.7;
+    camera.position.z = -0.55;
   })
 
   return (
     <>
-      <primitive object={model.scene} position={[0, -0.5, 0]} />
+      <primitive object={model.scene} position={[0.25, -0.55, 0]}  rotation={[0, Math.PI/3.9, 0]}/>
 
 
       <directionalLight position={[0, 5, 5]} color={0xffffff} intensity={10} />
