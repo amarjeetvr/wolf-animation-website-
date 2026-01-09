3ce07828ca28908867429b30d4895923f05ded4a
amarjeetvr <av457508@gmail.com>
2026-01-09 14:24:22 +0530

    animation potion and rotation set

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,11 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { Canvas, useThree } from '@react-three/fiber';
 import { OrbitControls, useGLTF, useTexture, useAnimations } from '@react-three/drei';
 import { texture } from 'three/tsl';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const Dog = () => {
-  // animation 3d
+
+  gsap.registerPlugin(useGSAP);
+  gsap.registerPlugin(ScrollTrigger);
+
+
   const model = useGLTF('/models/dog.drc.glb');
 
   useThree(({ camera, scene, gl }) => {
@@ -16,33 +23,30 @@ const Dog = () => {
   });
 
   const { actions } = useAnimations(model.animations, model.scene);
-  
+
   useEffect(() => {
     actions['Take 001'].play();
   }, [actions]);
-  // const textures = useTexture( {
-  //   normalMap: ('/dog_normals.jpg'),
-  //   sampleMatCap: ('/matcap/mat-2.png'),
-  // })
+
   const [
     normalMap,
     sampleMatCap
   ] = useTexture([
     '/dog_normals.jpg',
     '/matcap/mat-2.png',
-    
+
   ]).map(texture => {
     texture.flipY = false;
     texture.colorSpace = THREE.SRGBColorSpace;
     return texture;
   })
 
-  const [ branchMap, branchNormalMap ] = useTexture([
+  const [branchMap, branchNormalMap] = useTexture([
     '/branches_diffuse.jpg',
     '/branches_normals.jpg',
 
   ]).map(texture => {
-   
+
     texture.colorSpace = THREE.SRGBColorSpace;
     return texture;
   });
@@ -52,20 +56,55 @@ const Dog = () => {
     matcap: sampleMatCap
   });
 
-const branchMaterial = new THREE.MeshMatcapMaterial({
-  normalMap: branchNormalMap,
-  map: branchMap,
+  const branchMaterial = new THREE.MeshMatcapMaterial({
+    normalMap: branchNormalMap,
+    map: branchMap,
 
-});
+  });
 
   model.scene.traverse((child) => {
     if (child.name.includes("DOG")) {
       child.material = dogMaterial;
-      
-    }else{
+
+    } else {
       child.material = branchMaterial;
     }
   })
+
+
+  const dogModel = useRef(model)
+
+  useGSAP(() => {
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#section-1',
+        endTrigger: '#section-3',
+        start: 'top top',
+        end: 'bottom bottom',
+        markers: true,
+        scrub: true,
+      }
+    })
+    tl
+      .to(dogModel.current.scene.position, {
+        z: '-=0.75',
+        y: '+=0.1',
+      })
+      .to(dogModel.current.scene.rotation, {
+        x: `+=${Math.PI / 15}`,
+      })
+      .to(dogModel.current.scene.rotation, {
+        y: `-=${Math.PI}`,
+      }, "third")
+      .to(dogModel.current.scene.position, {
+        x: '-=0.5',
+        z: '+=0.6',
+        y: '-=0.1',
+      }, "third")
+
+  }, []);
+
 
 
   return (
@@ -74,7 +113,7 @@ const branchMaterial = new THREE.MeshMatcapMaterial({
 
 
       <directionalLight position={[0, 5, 5]} color={0xffffff} intensity={10} />
-  
+
     </>
   )
 }
